a851d658e61b6376ee01e5dc04209c064f074564
Ian Kottman <ikottman@users.noreply.github.com>
2019-08-19 09:16:10 -0500

    Change shards based on request path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,18 @@ class UsersController < ApplicationController
     render json: response
   end
 
+  def show
+    response = User.find_by last_name: params[:id]
+    render json: response
+  end
+
   def choose_shard
-    ActiveRecord::Base.connected_to(role: :writing) do
+    # default to shard 0
+    db = :shard0
+    db = :shard1 if request.path.include? "one"
+    puts "connecting to: #{db}"
+
+    ActiveRecord::Base.connected_to(database: db) do
       yield
     end
   end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  # writing and reading are the labels Rails assumes,
+  # we are not using them as replicas but rather as full shards.
   connects_to database: { writing: :shard0, reading: :shard1 }
 end
